6139b647c850a95041e03ca486e7704cf489e5e2
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-01-30 00:53:44 -0300

    Component: asideNote

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { MainDiv } from "./App"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <MainDiv>
+      <div className="aside">
+          <strong>Caderno de notas</strong>
+
+          <form>
+            <div className="inputBlock">
+              <label htmlFor="title">Titulo da anotação</label>
+              <input className="title" type="text" />
+
+            </div>
+          
+            <div className="inputBlock">
+              <label htmlFor="nota">Anotações</label>
+              <input className="anotatiton" type="text" />
+
+            </div>
+
+            <button type="submit"> Salvar</button>
+          </form>
+
+        <div className="main">
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </MainDiv>
   )
 }
 
-export default App
+export default App;
